2609f953f4d7d4e7227f7762cb90c73615ffcbe2
Alessandro0418 <aledigiannangonio@gmail.com>
2025-07-04 14:56:50 +0200

    Initial commit

---
+++ src/main/java/it/epicode/Capstone_Project_Backend/CapstoneProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.Capstone_Project_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/Capstone_Project_Backend/CapstoneProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Capstone_Project_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
